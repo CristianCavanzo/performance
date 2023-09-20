182ff81e81c222573937d5de32bbd97539822610
Cristian Cavanzo <cristian.frontendev@gmail.com>
2023-09-20 04:43:28 -0500

    [Add]: audit

--- src/modal/index.js
+++ src/modal/index.js
@@ -1,5 +1,6 @@
 import ModalVideo from 'modal-video'
 import 'modal-video/css/modal-video.min.css'
+
 const openModal = idYoutube => {
   const button = document.createElement('button')
   button.dataset.videoId = idYoutube
